24d9128e7dae780db83140ec82f1222e117365ff
Rohit Sharma <rohit.sharma1@news.co.uk>
2017-09-05 07:54:04 +0530

    My First commit.

---
+++ CreatingRemoteDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CreatingRemoteDemo
+//
+//  Created by Sharma, Rohit on 9/5/17.
+//  Copyright © 2017 Sharma, Rohit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
